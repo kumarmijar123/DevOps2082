c6d7fc17f843c4074fc0a87e346370285b7ec7e6
Admin <mijarkumar2019@gmail.com>
2025-07-19 17:25:39 +0545

    adding jar files

--- src/dbutil/DBUtil.java
+++ src/dbutil/DBUtil.java
@@ -5,20 +5,31 @@ import java.sql.*;
 
 public class DBUtil {
 
-public static Connection getConnection() {
-    Connection conn = null;
-    try {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        conn = DriverManager.getConnection(
-            "jdbc:mysql://localhost:3306/productmanagementsystem", 
-            "root", 
-            "123@12Kumar"
-        );
-        System.out.println("Database connected successfully.");
-    } catch (Exception e) {
-        System.out.println("Database connection failed!");
-        e.printStackTrace();
-    }
-    return conn;
-}
+	public static Connection getConnection()
+	{
+		Connection conn = null;
+		try 
+		{
+			
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		    conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/productmanagementsystem", "root", "123@12Kumar");
+		
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return conn;
+	}
+	
+	
+	public static void closeConnection(Connection conn)
+	{
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
